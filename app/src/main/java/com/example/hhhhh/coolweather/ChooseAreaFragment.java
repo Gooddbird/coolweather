7385042a50bfd04361c21f72d76a6f233a9e1796
HD <Hd@gmail.com>
2018-10-03 01:42:38 +0800

    加入天气更新与背景图

--- app/src/main/java/com/example/hhhhh/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/hhhhh/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.hhhhh.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -59,7 +60,6 @@ public class ChooseAreaFragment extends Fragment {
         listView=(ListView)view.findViewById(R.id.list_view);
         adapter=new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
         listView.setAdapter(adapter);
-
         return view;
     }
     @Override
@@ -77,6 +77,12 @@ public class ChooseAreaFragment extends Fragment {
                     selectedCity=cityList.get(position);
                     queryCounties();
                     currentLevel=LEVEL_COUNTY;
+                }else if(currentLevel==LEVEL_COUNTY){
+                    String weatherId=countyList.get(position).getWeatherId();
+                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/com/example/hhhhh/coolweather/util/Utility.java
+++ app/src/main/java/com/example/hhhhh/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.hhhhh.coolweather.db.City;
 import com.example.hhhhh.coolweather.db.County;
 import com.example.hhhhh.coolweather.db.Province;
+import com.example.hhhhh.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -69,5 +71,17 @@ public class Utility {
             }
         }
         return false;
+    }
+    /*解析weather实体类*/
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
